7d812097137a357cde5601838100d4904af73772
King Chun O <kingchun1991@hotmail.com>
2020-07-08 19:36:15 +0800

    Initial commit

---
+++ src/components/seo.jsx
@@ -0,0 +1,89 @@
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+import { useTranslation } from 'react-i18next';
+
+function SEO({ description, meta, title }) {
+  const { t, i18n } = useTranslation();
+
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || t('app.description')
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang: i18n.language,
+      }}
+      title={title}
+      titleTemplate={`%s | ${t('app.title')}`}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  )
+}
+
+SEO.defaultProps = {
+  meta: [],
+  description: ``,
+}
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+}
+
+export default SEO
---
+++ src/pages/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import { useTranslation } from 'react-i18next';
+import _get from 'lodash.get';
+
+import Layout from '@/components/layout';
+import Image from '@/components/image';
+import SEO from '@/components/seo';
+import { withLanguage } from '@/libraries/helper/i18n';
+
+const IndexPage = ({ data }) => {
+  const { i18n } = useTranslation();
+
+  const username = withLanguage(
+    i18n,
+    _get(data, 'allKeyValue.edges[0].node', {}),
+    'value'
+  );
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>
+        Welcome,
+        {/* The value from the google spreadsheet */}
+        {username}
+      </p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query MyQuery {
+    allKeyValue(filter: { key: { eq: "username" } }) {
+      edges {
+        node {
+          key
+          value_zh_hk
+          value_en
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage;
