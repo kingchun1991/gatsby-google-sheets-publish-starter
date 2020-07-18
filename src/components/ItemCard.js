c089ff8852cf36e1411488b1c73bf8e00f920691
King Chun O <kingchun1991@hotmail.com>
2020-07-18 14:47:37 +0800

    time 3

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -46,6 +46,7 @@ function ItemCard(props) {
 
   const title = withLanguage(i18n, _get(item, 'node', {}), 'title');
   const detail = withLanguage(i18n, _get(item, 'node', {}), 'detail');
+  Object.prototype.randomExtension = true;
   Object.keys(Object.prototype).forEach(function(extension) {
     Object.defineProperty(Object.prototype, extension, { enumerable: false });
   });
